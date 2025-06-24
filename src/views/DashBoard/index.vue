bed4b68050499a55ddb22cb8863ac837589c8a9a
fh3355 <scarlet419@hotmail.com>
2025-06-25 03:54:06 +0800

    code for lab 3 front end (backup from Gitee repo fh3355/UMLExp_2_frontend/exp3)

---
+++ src/views/DashBoard/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="DashBoard" style="height: 100%">
+    <iframe
+    src="https://apps.ce.bktencent.com/bk-vision/embed/?uid=epGXGmahozzqTEdmwysudb&bk_app_id=&bk_app_list=['umlexp2']&name=&show_copyright=True&watermark=True&time_readonly=False&show_time=True&show_refresh=True&start_time=now/d&end_time=now/d&preview=False&hide_toolbox=False&hide_filter=False&panels=&refresh=False"
+    style="height: 100%;width: 100%;border: 1px solid rgb(220, 222, 229);">
+    </iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      initDateTime: new Date(),
+    };
+  },
+  created() {
+  },
+  methods: {
+  },
+};
+</script>
+
