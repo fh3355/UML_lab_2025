a3d476a5477f24f35b7c41b08e6d063c6da6db08
fh3355 <scarlet419@hotmail.com>
2025-06-25 00:13:24 +0800

    code for lab 2 back end (backup from Gitee repo fh3355/UMLExp_2_backend/re)

--- local_settings.py
+++ local_settings.py
@@ -1,9 +1,9 @@
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "umlexp2",  # noqa
+        "NAME": "lesson11",  # noqa
         "USER": "root",
-        "PASSWORD": "fh3355",
+        "PASSWORD": "huan0912",
         "HOST": "localhost",
         "PORT": "3306",
     },
