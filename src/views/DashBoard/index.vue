39ccb837448dcad439e3e7b33ae54f63f744a2f8
fh3355 <scarlet419@hotmail.com>
2025-06-25 11:25:19 +0800

    code for lab 4 front end (backup from Gitee repo fh3355/UMLExp_2_frontend/exp4)

--- src/views/DashBoard/index.vue
+++ src/views/DashBoard/index.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="DashBoard" style="height: 100%">
+    
     <iframe
-    src="https://apps.ce.bktencent.com/bk-vision/embed/?uid=epGXGmahozzqTEdmwysudb&bk_app_id=&bk_app_list=['umlexp2']&name=&show_copyright=True&watermark=True&time_readonly=False&show_time=True&show_refresh=True&start_time=now/d&end_time=now/d&preview=False&hide_toolbox=False&hide_filter=False&panels=&refresh=False"
+    src="http://apps.ce.bktencent.com/bk-vision/embed/?uid=37atrwsX6RqRqH5MFVfpj6&bk_app_id=&bk_app_list=['umlexp2']&name=&show_copyright=True&watermark=True&time_readonly=False&show_time=True&show_refresh=True&start_time=now/d&end_time=now/d&preview=False&hide_toolbox=False&hide_filter=False&panels=&refresh=False"
     style="height: 100%;width: 100%;border: 1px solid rgb(220, 222, 229);">
     </iframe>
+
   </div>
 </template>
 
